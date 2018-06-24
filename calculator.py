e481acc5ab3136711a25501bd08945db5ef8c646
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-24 16:32:19 -0700

    added prettiness

--- calculator.py
+++ calculator.py
@@ -10,8 +10,11 @@ from arithmetic import *
 # Your code goes here
 
 # take input and split it into a list (tokenize)
+print("Welcome to the calculator!")
+
 while True:
-	user_input = input()
+
+	user_input = input("Please enter a command followed by the appropriate arguments \n >>> ")
 
 	token = user_input.split(" ")
 
@@ -22,42 +25,52 @@ while True:
 		break
 		pass	
 
-	if len(token) == 2:
-		print("Please enter in a valid input")
+	error_message = "Please enter in a valid input."
+
+
+	if len(token) < 3:
+		print(error_message)
+		continue
+
+	if len(token) > 4:
+		print(error_message)
 		continue
 
 	num1 = int(token[1])
 	num2 = int(token[2])
 
-
-	if len(token) <=3:
+	if len(token) == 3:
 		if operator == "+":
-			print(add(num1, num2))
-		if operator == "-":
-			print(subtract(num1, num2))
-		if operator == "*":
-			print(multiply(num1, num2))
-		if operator == "/":
-			print(divide(num1, num2))
-		if operator == "square":
-			print(square(num1, num2))
-		if operator == "cube":
-			print(cube(num1, num2))
-		if operator == "pow":
-			print(power(num1, num2))
-		if operator == "mod":
-			print(mod(num1, num2))
-		if operator == "cube+":
-			print(add_cubes(num1, num2))
+			result = add(num1, num2)
+		elif operator == "-":
+			result = subtract(num1, num2)
+		elif operator == "*":
+			result = multiply(num1, num2)
+		elif operator == "/":
+			result = divide(num1, num2)
+		elif operator == "square":
+			result = square(num1, num2)
+		elif operator == "cube":
+			result = cube(num1, num2)
+		elif operator == "pow":
+			result = power(num1, num2)
+		elif operator == "mod":
+			result = mod(num1, num2)
+		elif operator == "cube+":
+			result = add_cubes(num1, num2)
+		else:
+			result = error_message
 
 	if len(token) == 4:
 		num3 = int(token[3])
 		if operator == "x+":
-			print(add_mult(num1, num2, num3))
+			result = add_mult(num1, num2, num3)
+		else:
+			result = error_message
 
-	if len(token) > 4:
-		print("Please enter in a valid input")
+
 		
+	print(" >>> {}".format(result))
 
 
 

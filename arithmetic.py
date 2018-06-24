eb0c424114701d3fe22e98e3514b59ad64262840
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-24 15:59:11 -0700

    implemented numerous calculator functions

--- arithmetic.py
+++ arithmetic.py
@@ -4,50 +4,54 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return num1 + num2
+    return int(num1 + num2)
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
-    return num1 - num2
+    return int(num1 - num2)
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-    return num1 * num2
+    return int(num1 * num2)
 
 
 def divide(num1, num2):
-    """Divide the first input by the second, returning a floating point."""
+    """Divide the first input by the second and return the result."""
 
     return num1 / num2
 
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+
+    return int(num1 ** num2)
 
 def square(num1):
     """Return the square of the input."""
 
-    # Needs only one argument
-
-    return num1 * num1
+    return power(num1, 2)
 
 
 def cube(num1):
     """Return the cube of the input."""
 
-    # Needs only one argument
-
-    return num1 * num1 * num1
-
-
-def power(num1, num2):
-    """Raise num1 to the power of num and return the value."""
-
-    return num1 ** num2  # ** = exponent operator
+    return power(num1, 3)
 
 
 def mod(num1, num2):
-    """Return the remainder of num / num2."""
+    """Return the remainder of num1 / num2."""
 
-    return num1 % num2
+    return int(num1 % num2)
+
+def add_mult(num1, num2, num3):
+    """Add the first two and multiply sum with third"""
+
+    return int(multiply(add(num1, num2), num3))
+
+def add_cubes(num1, num2):
+	"""Return the sum of the cube of two numbers"""
+
+	return int(add(cube(num1), cube(num2)))
--- calculator.py
+++ calculator.py
@@ -16,44 +16,49 @@ while True:
 	token = user_input.split(" ")
 
 
-
-
-	if token[0] == "q":
-		pass	
-		#quit
-		
-	if len(token) <=3:
-		if token[0] == "+":
-			#call the add function
-			pass
-		if token[0] == "-":
-			#call the subtr function
-			pass
-		if token[0] == "*":
-			#call the multip function
-			pass
-		if token[0] == "/":
-			#call the div function
-			pass
-		if token[0] == "square":
-			#call the square funtion
-			pass
-		if token[0] == "cube":
-			#call the cube function
-			pass
-		if token[0] == "pow":
-			#call the power function
-			pass
-
-		if token[0] == "mod":
-			#call the modulo function
-			pass
-
-
-
-
-
 #the 0'th word in the token is the operator
+	operator = token[0]
+	num1 = int(token[1])
+	num2 = int(token[2])
+
+	if len(token) == 4:
+		num3 = int(token[3])
+		if operator == "x+":
+			print(add_mult(num1, num2, num3))
+
+	if len(token) > 4:
+		#print(error_message)
+		pass
+
+
+		if operator == "q":
+			pass	
+			#quit
+
+	if len(token) <=3:
+		if operator == "+":
+			print(add(num1, num2))
+		if operator == "-":
+			print(subtract(num1, num2))
+		if operator == "*":
+			print(multiply(num1, num2))
+		if operator == "/":
+			print(divide(num1, num2))
+		if operator == "square":
+			print(square(num1, num2))
+		if operator == "cube":
+			print(cube(num1, num2))
+		if operator == "pow":
+			print(power(num1, num2))
+		if operator == "mod":
+			print(mod(num1, num2))
+		if operator == "cube+":
+			print(add_cubes(num1, num2))
+
+
+
+
+
 
 
 # the operator determines which command(function) to use
